a318a10e2448df64bd70b063ad2bb91997376a8c
Łukasz Plust <84743879+lukaszplust@users.noreply.github.com>
2022-06-22 17:38:51 +0200

    Update 2.2.asm

--- 2.2.asm
+++ 2.2.asm
@@ -1,5 +1,6 @@
 .686
 .model flat
+<<<<<<< HEAD
 public _main
 extern _ExitProcess@4 : proc
 .data
@@ -8,5 +9,40 @@ extern _ExitProcess@4 : proc
 _main PROC
 	push 0
 	call _ExitProcess@4
+_main ENDP
+END
+=======
+extern _ExitProcess@4 : PROC
+extern _MessageBoxA@16 : PROC
+extern _MessageBoxW@16 : PROC
+public _main
+.data
+tytul_Unicode	dw 'T','e','k','s','t',' ','w',' '
+				dw 'U','T','F','-','1','6', 0
+tekst_Unicode	dw 'K','a',017Ch,'d','y',' ','z','n','a'
+				dw 'k',' ','t','o',' ','1','6',' '
+				dw 'b','i','t',00F3h,'w', 0
+tytul_Win1250	db 'Tekst w standardzie windows 1250', 0
+tekst_Win1250	db 'Ka',0BFh,'dy znak zajmuje '
+				db '8',' bit',0F3h,'w', 0
+.code
+_main PROC
+
+push 0
+push OFFSET tytul_Unicode
+push OFFSET tekst_Unicode
+push 0
+
+call _MessageBoxW@16
+
+push 0
+push OFFSET tytul_Win1250
+push OFFSET tekst_Win1250
+push 0
+
+call _MessageBoxA@16
+push 0
+call _ExitProcess@4
+
 _main ENDP
 END
