e47f82cd20ecd84dee91be3e351117616a5f74f8
Łukasz Plust <84743879+lukaszplust@users.noreply.github.com>
2022-06-22 17:41:59 +0200

    Update 2.2.asm

--- 2.2.asm
+++ 2.2.asm
@@ -1,20 +1,10 @@
 .686
 .model flat
-<<<<<<< HEAD
-public _main
-extern _ExitProcess@4 : proc
-.data
 
-.code
-_main PROC
-	push 0
-	call _ExitProcess@4
-_main ENDP
-END
-=======
 extern _ExitProcess@4 : PROC
 extern _MessageBoxA@16 : PROC
 extern _MessageBoxW@16 : PROC
+
 public _main
 .data
 tytul_Unicode	dw 'T','e','k','s','t',' ','w',' '
@@ -34,15 +24,17 @@ push OFFSET tekst_Unicode
 push 0
 
 call _MessageBoxW@16
-
 push 0
+
 push OFFSET tytul_Win1250
 push OFFSET tekst_Win1250
 push 0
 
 call _MessageBoxA@16
+
 push 0
 call _ExitProcess@4
 
 _main ENDP
+
 END
