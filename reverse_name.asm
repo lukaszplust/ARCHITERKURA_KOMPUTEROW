56c382fc060b28668feeb0b38e53ce2765947526
Łukasz Plust <84743879+lukaszplust@users.noreply.github.com>
2022-10-12 12:51:06 +0200

    Update reverse_name.asm

--- reverse_name.asm
+++ reverse_name.asm
@@ -1,92 +1,104 @@
 .686
 .model flat
-extern _ExitProcess@4 : PROC
-extern __write : PROC
-extern __read : PROC
-public _main
 
+extern _ExitProcess@4 : PROC
+extern __write : PROC ; (dwa znaki podkreślenia)
+extern __read : PROC ; (dwa znaki podkreślenia)
+
+public _main
 .data
-tekst_output db 10,'Prosze napisac swoje imie i nazwisko :',10
-koniec_tekstu db ?
-magazyn_input db 80 dup (?)
+tekst_pocz db 10, 'napisz imie i nazwisko',10
+koniec_t db ?
+
 magazyn_imie db 80 dup (?)
 magazyn_nazwisko db 80 dup (?)
+magazyn_inputu db 80 dup (?)
 magazyn_output db 80 dup (?)
+
+
 liczba_znakow dd ?
 
 .code
+_main PROC
+; wyświetlenie tekstu informacyjnego
 
-_main proc
-; liczba znakow tekstu
-mov ecx, (OFFSET koniec_tekstu) - (OFFSET tekst_output)
-
-; tekst output
+; liczba znaków tekstu
+mov ecx,(OFFSET koniec_t) - (OFFSET tekst_pocz)
 push ecx
-push OFFSET tekst_output
-push 1
-call __write
-add esp, 12
+push OFFSET tekst_pocz ; adres tekstu
+push 1 ; nr urządzenia (tu: ekran - nr 1)
+call __write ; wyświetlenie tekstu początkowego
+add esp, 12 ; usuniecie parametrów ze stosu
+
+; czytanie wiersza z klawiatury
+push 80 ; maksymalna liczba znaków
+push OFFSET magazyn_inputu
+push 0 ; nr urządzenia (tu: klawiatura - nr 0)
+call __read ; czytanie znaków z klawiatury
+add esp, 12 ; usuniecie parametrów ze stosu
 
 
-; tekst input
-push 80
-push OFFSET magazyn_input
-push 0
-call __read
-add esp, 12
-;__read wpisuje do eax liczbe wprowadzonych znakow
 
-mov liczba_znakow, eax ; funkcja read wpisuje do eax liczbe wprowadzonych znakow
-mov ecx, eax ; ecx bedzie licznikiem calego inputu
-mov esi, 0 ; esi jest indeksem inputu
-mov eax, 0 ; eax jest indeksem imienia lukasz = 6
-mov ebx, 0 ; ebx jest indeksem nazwiska plust = 5
-mov edi, 0 ; edi jest indeksem outputu
+mov liczba_znakow, eax
+mov ecx, eax;	ecx -> licznik petli
+mov eax,0;		eax -> indeks imienia
+mov edi,0;		edi -> indeks nazwiska
+mov ebx,0;		ebx -> indeks inputu
+mov esi,0;		esi -> indeks outputu
 
-ptl: mov dl, magazyn_input[esi]
+;dodawanie liter z imienia
+ptl:
+	mov dl, magazyn_inputu[ebx]
+	inc ebx
+	cmp dl,' '
+	je addNazwisko
+	mov magazyn_imie[eax], dl
+	inc eax
+	loop ptl
+
+;dodawanie liter z nazwiska
+addNazwisko:
+	mov dl, magazyn_inputu[ebx]
+	inc ebx
+	mov magazyn_nazwisko[edi],dl
+	inc edi
+	loop addNazwisko
+
+;przeniesienie indeksów nazwiska do licznika ecx i odjecie spacji i entera od licznika
+mov ecx, edi
+mov edi,0
+sub ecx,2
+
+doOutputuImie:
+	mov dl, magazyn_nazwisko[edi]
+	inc edi
+	mov magazyn_output[esi], dl
+	inc esi
+	loop doOutputuImie
+
+mov magazyn_output[esi], ' '
 inc esi
-cmp dl, ' '
-je nazwisko
-mov magazyn_imie[eax], dl;dajemy daną literke do magazyn_imie[eax]
-inc eax
-loop ptl
 
-nazwisko: mov dl, magazyn_input[esi]
-inc esi
-mov magazyn_nazwisko[ebx], dl
-inc ebx
+mov ecx,eax
+mov eax,0
 
-loop nazwisko
-mov ecx, ebx
-sub ecx, 2 ; spacja oraz enter z inputu
-mov ebx, 0
+doOutputuNazwisko:
+	mov dl,magazyn_imie[eax]
+	inc eax
+	mov magazyn_output[esi],dl
+	inc esi
+	loop doOutputuNazwisko
 
-dodajNazwisko: mov dl, magazyn_nazwisko[ebx];dodajemy nazwisko do wyswietlenia
-inc ebx
-mov magazyn_output[edi], dl
-inc edi
-loop dodajNazwisko
 
-mov magazyn_output[edi], ' '
-inc edi
-mov ecx, eax
-mov eax, 0
 
-dodajImie: mov dl, magazyn_imie[eax];dodajemy imie do wyswietlenia
-inc eax
-mov magazyn_output[edi], dl
-inc edi
-loop dodajImie
-  
-; wyswietlanie tekstu
+
+
 push liczba_znakow
 push OFFSET magazyn_output
 push 1
-call __write
-add esp, 12
-
-; zakonczenie programu
+call __write ; wyświetlenie przekształconego tekstu
+add esp, 12 ; usuniecie parametrów ze stosu
 push 0
-call _ExitProcess@4
-_main endp
+call _ExitProcess@4 ; zakończenie programu
+_main ENDP
 END
