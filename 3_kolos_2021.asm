a9af73c6fb913e3c9aea3f4dfbda765da4eda538
Łukasz Plust <84743879+lukaszplust@users.noreply.github.com>
2022-12-11 20:36:28 +0100

    Update 3_kolos_2021.asm

--- 3_kolos_2021.asm
+++ 3_kolos_2021.asm
@@ -17,17 +17,17 @@ _main PROC
 xor eax,eax
 xor ebx,ebx
 lea esi,znak
-mov dl,[esi+2]
+mov dl,[esi+0]
 mov bl,[esi+3]
 xchg dl,bl
-mov [esi+2],dl
+mov [esi+0],dl
 mov [esi+3],bl
 
-mov dl,[esi+0]
-mov bl,[esi+1]
+mov dl,[esi+1]
+mov bl,[esi+2]
 xchg dl,bl
-mov [esi+0],dl
-mov [esi+1],bl
+mov [esi+1],dl
+mov [esi+2],bl
 
 
 
