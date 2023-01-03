a6e5650c145df8ec55067d06acee81c4b11370c1
Łukasz Plust <84743879+lukaszplust@users.noreply.github.com>
2023-01-03 21:24:44 +0100

    Create kolos_2_NWD.c

---
+++ kolos_2_NWD.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+unsigned int NWD(unsigned int a, unsigned int b);
+
+int main()
+{
+    unsigned int a = 8, b = 6;
+    unsigned int wynik = NWD(a,b);
+    printf("Wynik to:%d", wynik);
+
+
+    return 0;
+}
