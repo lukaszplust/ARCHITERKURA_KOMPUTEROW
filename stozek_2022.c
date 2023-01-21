ccb16482e9d0fb9c85b7f2314f925b1bc03e19fc
Łukasz Plust <84743879+lukaszplust@users.noreply.github.com>
2023-01-21 23:20:13 +0100

    Create stozek_2022.c

---
+++ stozek_2022.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+float objetosc_stozka(unsigned int big_r, float small_r, float h);
+
+int main() {
+
+	float wynik = objetosc_stozka(6, 2.0, 5.3);
+	printf("\nWynik = %f", wynik);
+
+	return 0;
+}
